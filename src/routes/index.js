5bbf6e8c55d4c923060a921076d65e1d6a412622
Omar <omarafjo@gmail.com>
2024-11-19 07:49:07 -0600

    Added paths in router index

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,20 @@ router.get('/', (req, res) => {
     res.sendFile('public/views/home.html', { root: '.' });
 });
 
+router.get('/home', (req, res) => {
+    res.sendFile('public/views/home.html', { root: '.' });
+});
+
+router.get('/contact', (req, res) => {
+    res.sendFile('public/views/contact.html', { root: '.' });
+});
+
 router.get('/chats', (req, res) => {
     res.sendFile('public/views/chat_index.html', {root: '.'});
 })
+
+router.get('/chat/Firulais', (req, res) => {
+    res.sendFile('public/views/chat.html', {root: '.'});
+});
 
 module.exports = router;
