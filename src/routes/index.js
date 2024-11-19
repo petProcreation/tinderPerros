50c0ae04cb2c69d3f7f4fd595368f612b5c54e99
Omar <omarafjo@gmail.com>
2024-11-19 07:32:54 -0600

    added chat_index

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
     res.sendFile('public/views/home.html', { root: '.' });
 });
 
+router.get('/chats', (req, res) => {
+    res.sendFile('public/views/chat_index.html', {root: '.'});
+})
+
 module.exports = router;
