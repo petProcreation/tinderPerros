b7fc029f069e20e32de6ada1c0cd9eb4154cca8c
Omar <omarafjo@gmail.com>
2024-11-25 19:03:12 -0600

    aaa

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -15,7 +15,7 @@ async function createUser(req, res) {
 // Get all users
 async function getUsers(req, res) {
     try {
-        const users = await User.find({});
+        const users = await User.find();
         res.status(200).send(users);
     } catch (error) {
         res.status(500).send(error);
